5376cf348b53bd92c9abc8dbb2da3ca93329f7f6
rafambn <rafa_mbn@hotmail.com>
2026-01-21 19:35:06 -0300

    Refactor `KeystoreScreen` to use ViewModel factory and simplify initialization.

--- composeApp/src/jvmMain/kotlin/unidesk/com/br/keymanager/ui/keystore/KeystoreScreen.kt
+++ composeApp/src/jvmMain/kotlin/unidesk/com/br/keymanager/ui/keystore/KeystoreScreen.kt
@@ -3,12 +3,11 @@ package unidesk.com.br.keymanager.ui.keystore
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
-import unidesk.com.br.keymanager.core.KeystoreRepository
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun KeystoreScreen() {
-    val viewModel = remember { KeystoreViewModel(repository = KeystoreRepository()) }
+    val viewModel = viewModel<KeystoreViewModel>(factory = KeystoreViewModel.Factory)
     val state = viewModel.state.collectAsState()
 
     LaunchedEffect(viewModel) {
